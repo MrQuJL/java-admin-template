887219699453b3848dc2614eb25ee3446f91641a
Jianlei Qu <lyu_qujianlei@163.com>
2025-12-29 09:04:13 +0800

    update

--- src/main/java/com/example/admin/mapper/DepartmentMapper.java
+++
@@ -1,9 +0,0 @@
-package com.example.admin.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.example.admin.entity.Department;
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface DepartmentMapper extends BaseMapper<Department> {
-}
